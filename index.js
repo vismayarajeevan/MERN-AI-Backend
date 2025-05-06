033a1b336b57ff45e1d95f9eb44f94160af9e5a7
vismayarajeevan <vismayarajeevan15@gmail.com>
2025-05-06 16:48:22 +0530

    index file created

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv= require('dotenv')
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`Server start running on PORT ${PORT}`);
+    
+})
